e51f6e836cae463b1c5abbed2ef8b50ee6a0d528
Henzgo <129070530+Henzgo@users.noreply.github.com>
2026-09-18 00:52:41 +0200

    Start implementing face detection, add files and folders.

--- Face_detector.cpp
+++ Face_detector.cpp
@@ -4,9 +4,54 @@
 #include "Face_detector.h"
 
 using namespace std;
+using namespace cv;
 
-int main()
+void detectAndDraw(Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedCascade, double scale);
+string cascadeName, nestedCascadeName;
+
+int main(int argc, const char** argv)
 {
-	cout << "Hello CMake." << endl;
+	VideoCapture capture;
+	Mat frame, image;
+
+	CascadeClassifier cascade, nestedCascade;
+	double scale = 1;
+	nestedCascade.load("resources/haarcascade_eye_tree_eyeglasses.xml");
+	nestedCascade.load("resources/haarcascade_frontalface_default.xml");
+
+	capture.open(0);
+	if (capture.isOpened()) {
+		std::cout << "Face Detection Started..." << std::endl;
+		while (1) {
+			capture frame;
+			if (frame.empty())
+				break;
+			Mat frame1 = frame.clone();
+			detectAndDraw(frame1, cascade, nestedCascade, scale);
+			char c = (char)waitKey(10);
+
+			if (c == 27 || c == 'q' || c == 'Q')
+				break;
+		}
+	}
+	else
+		std::cout << "Could not open Camera" << std::endl;
 	return 0;
+}
+
+void detectAndDraw(Mat img, CascadeClassifier cascade, CascadeClassifier nestedCascade, double scale) {
+	vector Rect, faces, faces2;
+	Mat gray, smallImg;
+
+	cvtColor(img, gray, COLOR_BGR2GRAY);
+	double fx = 1 / scale;
+
+	resize(gray, smallImg, Size(), fx, fx, INTERN_LINEAR);
+	equalizeHist(smallImg, smallImg);
+
+	cascade.detectMultiScale(smallImg, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+
+	for (size_t i = 0; i < faces.size(); i++) {
+		Rect rand = faces[i];
+	})
 }
--- Face_detector.h
+++ Face_detector.h
@@ -4,5 +4,9 @@
 #pragma once
 
 #include <iostream>
+#include <filesystem>
 
 // TODO: Verweisen Sie hier auf zusätzliche Header, die Ihr Programm erfordert.
+#include <opencv2/opencv.hpp>
+
+cv::CascadeClassifier faceCascade;
