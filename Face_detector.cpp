9737cb466e06c59bf4751dee9793efb361618092
Henry <deine-anonyme-adresse@users.noreply.github.com>
2026-09-17 22:41:22 +0200

    Projektdateien hinzufügen.

---
+++ Face_detector.cpp
@@ -0,0 +1,12 @@
+﻿// Face_detector.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "Face_detector.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
